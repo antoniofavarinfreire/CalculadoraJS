9c0efff5573ff19249d24902cb7e0dd553b17f86
Antonio Favarin <favarinfreire@hotmail.com>
2022-02-20 20:55:13 -0300

    Aula sobre manipulação do DOM

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,9 +2,20 @@ class CalcControler {
     
     constructor (){
         this._displayCalc = "0"; //Atributos privados (_atributo)
-        this._dataAtual; //Atributos, são parecidos com varaveis, porem, com mais funcionalidades
+        this._currentDate; //Atributos, são parecidos com varaveis, porem, com mais funcionalidades
+        this.initialize();
     }
 
+    initialize() {
+        let displayCalcEl = document.querySelector('#display');
+        let dateEl        = document.querySelector('#hora');
+        let timeEl        = document.querySelector('#data');
+
+        displayCalcEl.innerHTML = "4567";
+        dateEl.innerHTML = "20:52";
+        timeEl.innerHTML = "20/02/2022";
+
+    }
 
     get displayCalc() {
         return this._displayCalc;
@@ -14,12 +25,24 @@ class CalcControler {
         this._displayCalc = valor;
     }
 
-    get dataAtual() {
-        return this._dataAtual;
+    get currentDate() {
+        return this._currentDate;
     }
 
-    set dataAtual(valor) {
-        this._dataAtual = valor;
+    set currentDate(valor) {
+        this._currentDate = valor;
     }
 
 }
+
+/*
+    Manipular o DOM 
+    D: document
+    O: object
+    M: Model
+
+    BOM
+    B: browser 
+    O: object
+    M: Model
+*/
