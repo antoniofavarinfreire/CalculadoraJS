1ce40d5508bb9c76e056b0e72aaf8cf9973e38b3
Objetiva Digital <objetiva@Mac-mini-de-Objetiva.local>
2022-10-24 16:47:04 -0300

    opa

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,79 +1,100 @@
-class CalcControler{
+class CalcControler {
 
-    constructor (){
+  constructor() {
 
-        this._locale        = 'pt-BR';
-        this._displayCalcEl = document.querySelector("#display");
-        this._dateEl        = document.querySelector("#data");
-        this._timeEl        = document.querySelector("#hora");
-        this._diplayCalc; //Atributo privado
-        this._dataAtual;
-        this.initialize();
+    this._locale = 'pt-BR';
+    this._displayCalcEl = document.querySelector("#display");
+    this._dateEl = document.querySelector("#data");
+    this._timeEl = document.querySelector("#hora");
+    this._diplayCalc; //Atributo privado
+    this._dataAtual;
+    this.initialize();
+    this.initButtonsEvents();
+  }
 
-    }
+  initialize() {
 
-    initialize () {
+    this.setdisplayDateTime();
 
-        this.setdisplayDateTime();
+    setInterval(() => { //Utiliza para algo que seja intermitente 
 
-        setInterval(()=>{ //Utiliza para algo que seja intermitente 
+      this.setdisplayDateTime();
 
-            this.setdisplayDateTime();    
+    }, 1000); //1 seg
 
-        },1000); //1 seg
+  }
 
-    }
-    
-    initButtonsEvents(){
+  addEventListenerAll(elements, events, fn) {
 
-        document.querySelectorAll("#buttons > g, #parts > g"); // > Celetores filhos
+    events.split(" ").forEach(event => {
+      elements.addEventListener(event, fn, false);
+    });
 
-    }
-    
-    setdisplayDateTime(){
+  }
 
-        this.displayDate = this.currentDate.toLocaleDateString(this._locale,{ //Data baseada no local de origem
-            day: "2-digit",
-            month: "long",
-            year: "numeric"
-        });
-        this.displayTime = this.currentDate.toLocaleTimeString(this._locale); //Hora baseada no local de origem
-        
-    }
+  initButtonsEvents() {
 
-    get displayTime(){
-        return this._timeEl.innerHTML;
-    }
+    let buttons = document.querySelectorAll("#buttons > g, #parts > g"); // > Celetores filhos
 
-    get displayDate(){
-        return this._dateEl.innerHTML;
-    }
+    buttons.forEach((btn, index) => {
 
-    set displayTime(value){
-        return this._timeEl.innerHTML = value;
-    }
+      this.addEventListenerAll(btn, 'click drag ', e => {
 
-    set displayDate(value){
-        return this._dateEl.innerHTML = value;
-    }
+        console.log(btn.className.baseVal.replace("btn-", ""));
 
-    get displayCalc (){
+      });
 
-        return this._displayCalcEl.innerHTML;
+      this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+        btn.style.cursor = "pointer";
+      })
 
-    }
+    })
+  }
 
-    get currentDate(){
-        return new Date();
-    }
+  setdisplayDateTime() {
 
-    set displayCalc (value) {
+    this.displayDate = this.currentDate.toLocaleDateString(this._locale, { //Data baseada no local de origem
+      day: "2-digit",
+      month: "long",
+      year: "numeric"
+    });
+    this.displayTime = this.currentDate.toLocaleTimeString(this._locale); //Hora baseada no local de origem
 
-        this._displayCalc = value
-    }
+  }
 
-    set currentDate(value){
-        this._dataAtual = value
-    }
+  get displayTime() {
+    return this._timeEl.innerHTML;
+  }
+
+  get displayDate() {
+    return this._dateEl.innerHTML;
+  }
+
+  set displayTime(value) {
+    return this._timeEl.innerHTML = value;
+  }
+
+  set displayDate(value) {
+    return this._dateEl.innerHTML = value;
+  }
+
+  get displayCalc() {
+
+    return this._displayCalcEl.innerHTML;
+
+  }
+
+  get currentDate() {
+    return new Date();
+  }
+
+  set displayCalc(value) {
+
+    this._displayCalc = value
+  }
+
+  set currentDate(value) {
+    this._dataAtual = value
+  }
 
 }
